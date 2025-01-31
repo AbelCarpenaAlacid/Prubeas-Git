fa584fbaf9df93b4a12da37365a833a184cf72e5
Abel Carpena Alacid <1441967@alu.murciaeduca.es>
2025-01-31 10:14:28 +0100

    Gachasmigero V3

--- src/main/java/org/example/recetas/Gachamiguer.java
+++ src/main/java/org/example/recetas/Gachamiguer.java
@@ -2,13 +2,13 @@ package org.example.recetas;
 
 public class Gachamiguer {
     public static String hayGachasmigas(int x){
-        if(x%15==0){
+        if((x/15d) % 1==0){
             return "Gachamigas";
         }
-        if(x%3==0){
+        if((x/3d) % 1==0){
             return "Gacha";
         }
-        if(x%5==0){
+        if((x/5d) % 1==0){
             return "Migas";
         }
         return String.valueOf(x);
